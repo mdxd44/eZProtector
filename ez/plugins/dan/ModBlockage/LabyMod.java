7b4fdb18a663df180cf19d3e9e0bc9806b83bd6f
dvargas135 <dvargas135@gmail.com>
2016-08-12 11:06:32 -0500

    Create LabyMod.java

---
+++ ez/plugins/dan/ModBlockage/LabyMod.java
@@ -0,0 +1,7 @@
+package ez.plugins.dan.ModBlockage;
+
+public class LabyMod {
+	public enum EnumLabyModFeature {
+    	FOOD, GUI, NICK, BLOCKBUILD, CHAT, EXTRAS, ANIMATIONS, POTIONS, ARMOR, DAMAGEINDICATOR, MINIMAP_RADAR;
+    }
+}
