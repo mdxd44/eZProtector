85d2e683b6bd093d7aedfdcd5fd3894169c35dd9
dvargas135 <dvargas135@gmail.com>
2016-09-17 19:24:02 -0500

    Update Main.java

--- ez/plugins/dan/Main.java
+++ ez/plugins/dan/Main.java
@@ -32,129 +32,128 @@ public class Main extends JavaPlugin implements Listener {
 	public static final String WDLCONTROL = "WDL|CONTROL";
 	public static final String MCBRAND = "MC|Brand";
 	public static final String WDLREQ = "WDL|REQUEST";
-    public static final String SCHEMATICA = "schematica";
+	public static final String SCHEMATICA = "schematica";
 	public static ArrayList<String> plugins = new ArrayList<String>();
-    boolean setup = new Setup().setupEZP();
-    public static Plugin plugin;
-    private static JavaPlugin JavaPlugin;
-    private IPluginMessageListener pluginMessageListener = new IPluginMessageListener();
-    private List<String> blocked = new ArrayList<String>();
-    public static String prefix;
-    public static String player = "";
-    public static String playerCommand = "";
-    public static String errorMessage = "";
-    
-    public void onEnable() {
-    	plugin = this;
-    	log = getLogger();
-    	prefix = plugin.getConfig().getString("prefix");
-    	
-    	if (setup == true) {
-    		if (this.getConfig().getBoolean("updater")) {
-    			getServer().getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
-    				public void run() {
-    					UpdateChecker.checkUpdate();
-    				}
-    			}
-    			, 20L);
-    		}
-    		
-    		loadConfig();
-    		
-    		getServer().getMessenger().registerIncomingPluginChannel(this, ZIG, this.pluginMessageListener);
-    		getServer().getMessenger().registerIncomingPluginChannel(this, BSM, this.pluginMessageListener);
-    		getServer().getMessenger().registerIncomingPluginChannel(this, WDLINIT, this.pluginMessageListener);
-    		getServer().getMessenger().registerIncomingPluginChannel(this, MCBRAND,  this.pluginMessageListener);
-    		getServer().getMessenger().registerIncomingPluginChannel(this, WDLREQ, this.pluginMessageListener);
-    		getServer().getMessenger().registerOutgoingPluginChannel(this, ZIG);
-    		getServer().getMessenger().registerOutgoingPluginChannel(this, WDLCONTROL);
-    		getServer().getMessenger().registerOutgoingPluginChannel(this, BSM);
-    		getServer().getMessenger().registerOutgoingPluginChannel(this, SCHEMATICA);
-		  
-    		registerEvents(this, new IPlayerCommandPreprocessEvent(this), new IPlayerJoinEvent(this), new IPlayerLoginEvent(), new ISignChangeEvent(this));
-    		
-    		getCommand("ezp").setExecutor(new ICommandExecutor());
-		  
-    		MetricsChecker.tryMetrics();
-    		
-    		for (String string : getConfig().getStringList("block-commands.commands")) {
-    			this.blocked.add(string);
-    		}
-    		
-    		this.blocked.add("all");
-    		this.blocked.add("/plugins");
-    		this.blocked.add("/pl");
-    		this.blocked.add("/icanhasbukkit");
-    		this.blocked.add("/?");
-    		this.blocked.add("/version");
-    		this.blocked.add("/ver");
-    		this.blocked.add("/about");
-    		this.blocked.add("/a");
-    		this.blocked.add("/help");
-    		
-    		if (Bukkit.getPluginManager().isPluginEnabled("ProtocolLib")) {
-    			setupProtocolLibHooks(this.blocked);
-    		} else {
-    			log.warning("ProtocolLib not found.");
-    			log.warning("The plugin needs ProtocolLib to work.");
-    			log.warning("Disabling.");
-    			Bukkit.getPluginManager().disablePlugin(this);
-    		}
-    		for (String s : this.getConfig().getString("custom-plugins.plugins").split(", ")) {
-    			Main.plugins.add(s);
-    		}
-    		if (this.getConfig().getBoolean("log-blocked-mods")) {
-    			ModLogger.logMods();
-    		}
-    		log.info("Your server version is compatible with eZProtector.");
-    		log.info("Setting up...");
-    		log.info("The plugin has enabled successfully. Version: " + getDescription().getVersion());
-    	} else {
-    		log.warning("Your server version is not compatible with eZProtector.");
-    		Bukkit.getPluginManager().disablePlugin(this);
-    	}
-    }
-    public void onDisable() {
-    	log = getLogger();
-    	saveDefaultConfig();
+	boolean setup = new Setup().setupEZP();
+	public static Plugin plugin;
+	private static JavaPlugin JavaPlugin;
+	private IPluginMessageListener pluginMessageListener = new IPluginMessageListener();
+	private List<String> blocked = new ArrayList<String>();
+	public static String prefix;
+	public static String player = "";
+	public static String playerCommand = "";
+	public static String errorMessage = "";
+	
+	public void onEnable() {
+		plugin = this;
+		log = getLogger();
+		prefix = plugin.getConfig().getString("prefix");
+		
+		if (setup == true) {
+			if (this.getConfig().getBoolean("updater")) {
+				getServer().getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
+					public void run() {
+						UpdateChecker.checkUpdate();
+					}
+				}
+				, 20L);
+			}
+			
+			loadConfig();
+			
+			getServer().getMessenger().registerIncomingPluginChannel(this, ZIG, this.pluginMessageListener);
+			getServer().getMessenger().registerIncomingPluginChannel(this, BSM, this.pluginMessageListener);
+			getServer().getMessenger().registerIncomingPluginChannel(this, WDLINIT, this.pluginMessageListener);
+			getServer().getMessenger().registerIncomingPluginChannel(this, MCBRAND, this.pluginMessageListener);
+			getServer().getMessenger().registerIncomingPluginChannel(this, WDLREQ, this.pluginMessageListener);
+			getServer().getMessenger().registerOutgoingPluginChannel(this, ZIG);
+			getServer().getMessenger().registerOutgoingPluginChannel(this, WDLCONTROL);
+			getServer().getMessenger().registerOutgoingPluginChannel(this, BSM);
+			getServer().getMessenger().registerOutgoingPluginChannel(this, SCHEMATICA);
+			
+			registerEvents(this, new IPlayerCommandPreprocessEvent(this), new IPlayerJoinEvent(this), new IPlayerLoginEvent(), new ISignChangeEvent(this));
+			
+			getCommand("ezp").setExecutor(new ICommandExecutor());
+			
+			MetricsChecker.tryMetrics();
+			
+			for (String string : getConfig().getStringList("block-commands.commands")) {
+				this.blocked.add(string);
+			}
+			
+			this.blocked.add("all");
+			this.blocked.add("/plugins");
+			this.blocked.add("/pl");
+			this.blocked.add("/icanhasbukkit");
+			this.blocked.add("/?");
+			this.blocked.add("/version");
+			this.blocked.add("/ver");
+			this.blocked.add("/about");
+			this.blocked.add("/a");
+			this.blocked.add("/help");
+			
+			if (Bukkit.getPluginManager().isPluginEnabled("ProtocolLib")) {
+				setupProtocolLibHooks(this.blocked);
+			} else {
+				log.warning("ProtocolLib not found.");
+				log.warning("The plugin needs ProtocolLib to work.");
+				log.warning("Disabling.");
+				Bukkit.getPluginManager().disablePlugin(this);
+			}
+			for (String s : this.getConfig().getString("custom-plugins.plugins").split(", ")) {
+				Main.plugins.add(s);
+			}
+			if (this.getConfig().getBoolean("log-blocked-mods")) {
+				ModLogger.logMods();
+			}
+			log.info("Your server version is compatible with eZProtector.");
+			log.info("Setting up...");
+			log.info("The plugin has enabled successfully. Version: " + getDescription().getVersion());
+		} else {
+			log.warning("Your server version is not compatible with eZProtector.");
+			Bukkit.getPluginManager().disablePlugin(this);
+		}
+	}
+	public void onDisable() {
+		log = getLogger();
+		saveDefaultConfig();
 		getServer().getMessenger().unregisterIncomingPluginChannel(this, ZIG, this.pluginMessageListener);
 		getServer().getMessenger().unregisterIncomingPluginChannel(this, BSM, this.pluginMessageListener);
 		getServer().getMessenger().unregisterIncomingPluginChannel(this, WDLINIT, this.pluginMessageListener);
-		getServer().getMessenger().unregisterIncomingPluginChannel(this, MCBRAND,  this.pluginMessageListener);
+		getServer().getMessenger().unregisterIncomingPluginChannel(this, MCBRAND, this.pluginMessageListener);
 		getServer().getMessenger().unregisterIncomingPluginChannel(this, WDLREQ, this.pluginMessageListener);
 		getServer().getMessenger().unregisterOutgoingPluginChannel(this, ZIG);
 		getServer().getMessenger().unregisterOutgoingPluginChannel(this, WDLCONTROL);
 		getServer().getMessenger().unregisterOutgoingPluginChannel(this, BSM);
 		getServer().getMessenger().unregisterOutgoingPluginChannel(this, SCHEMATICA);
 		log.info("Plugin disabled.");
-    	plugin = null;
-    }
-    public static void registerEvents(org.bukkit.plugin.Plugin plugin, Listener... listeners) {
-    	for (Listener listener : listeners) {
-    		Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
-    	}
-    }
-    public static Plugin getPlugin() {
-    	return plugin;
-    }
-    public static JavaPlugin getJavaPlugin() {
-    	return JavaPlugin;
-    }
-    public void setupProtocolLibHooks(List<String> protocolList) {
-      IPacketEvent.protocolLibHook(protocolList);
-    }
-    private void loadConfig() {
-    	log = getLogger();
-    	FileConfiguration cfg = this.getConfig();
-    	cfg.options().copyDefaults(true);
-    	this.saveDefaultConfig();
-    	log.info("Reloading config...");
-    }
-    public static String placeholders(String args) {
-    	String newString = StringEscapeUtils.unescapeJava(
-    		args.replace("%prefix%", prefix).replace("%player%", player).replace("%errormessage%", errorMessage).replace("%command%", 
-    				new StringBuilder("/").append(playerCommand).toString()).replaceAll("&", "§"));
-        return newString;
-    }
-
+		plugin = null;
+	}
+	public static void registerEvents(org.bukkit.plugin.Plugin plugin, Listener... listeners) {
+		for (Listener listener : listeners) {
+			Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
+		}
+	}
+	public static Plugin getPlugin() {
+		return plugin;
+	}
+	public static JavaPlugin getJavaPlugin() {
+		return JavaPlugin;
+	}
+	public void setupProtocolLibHooks(List<String> protocolList) {
+		IPacketEvent.protocolLibHook(protocolList);
+	}
+	private void loadConfig() {
+		log = getLogger();
+		FileConfiguration cfg = this.getConfig();
+		cfg.options().copyDefaults(true);
+		this.saveDefaultConfig();
+		log.info("Reloading config...");
+	}
+	public static String placeholders(String args) {
+		String newString = StringEscapeUtils.unescapeJava(args.replace("%prefix%", prefix)
+				.replace("%player%", player).replace("%errormessage%", errorMessage)
+				.replace("%command%",new StringBuilder("/").append(playerCommand).toString()).replaceAll("&", "§"));
+		return newString;
+	}
 }
