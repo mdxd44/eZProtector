83110812f8fb8736ca37b10d0894122a03a23547
dvargas135 <dvargas135@users.noreply.github.com>
2017-06-27 13:29:08 -0500

    Updated for 1.12

--- ez/plugins/dan/Extras/Setup.java
+++ ez/plugins/dan/Extras/Setup.java
@@ -38,44 +38,47 @@ import ez.plugins.dan.Compatibility.R1_9_R1;
 import ez.plugins.dan.Compatibility.R1_9_R2;
 import ez.plugins.dan.Compatibility.R1_10_R1;
 import ez.plugins.dan.Compatibility.R1_11_R1;
+import ez.plugins.dan.Compatibility.R1_12_R1;
 
 public class Setup {
 	public static Compatibility IeZP;
 	public boolean setupEZP() {
 
-    	String version;
+		String version;
 
-    	try {
-    		version = Bukkit.getServer().getClass().getPackage().getName().replace(".",  ",").split(",")[3];
-    	} catch (ArrayIndexOutOfBoundsException whatVersionAreYouUsingException) {
-    		return false;
-    	}
-    	Logger.getLogger("Minecraft").info("[eZProtector] Your server is running version " + version);
-    	if (version.equals("v1_7_R1")) {
-    		IeZP = new R1_7_R1();
-    	} else if (version.equals("v1_7_R2")) {
-    		IeZP = new R1_7_R2();
-    	} else if (version.equals("v1_7_R3")) {
-    		IeZP = new R1_7_R3();
-    	} else if (version.equals("v1_7_R4")) {
-    		IeZP = new R1_7_R4();
-        } else if (version.equals("v1_8_R1")) {
-    		IeZP = new R1_8_R1();
-    	} else if (version.equals("v1_8_R2")) {
-    		IeZP = new R1_8_R2();
-    	} else if (version.equals("v1_8_R3")) {
-    		IeZP = new R1_8_R3();
-    	} else if (version.equals("v1_9_R1")) {
-    		IeZP = new R1_9_R1();
-    	} else if (version.equals("v1_9_R2")) {
-    		IeZP = new R1_9_R2();
-    	} else if (version.equals("v1_10_R1")) {
-    		IeZP = new R1_10_R1();
-    	} else if (version.equals("v1_11_R1")) {
-    		IeZP = new R1_11_R1();
-    	}
-    	// This will return true if the server version was compatible with one of our NMS classes
-    	// because if it is, our IeZP would not be null
-    	return IeZP != null;
-    }
+		try {
+			version = Bukkit.getServer().getClass().getPackage().getName().replace(".",  ",").split(",")[3];
+		} catch (ArrayIndexOutOfBoundsException whatVersionAreYouUsingException) {
+			return false;
+		}
+		Logger.getLogger("Minecraft").info("[eZProtector] Your server is running version " + version);
+		if (version.equals("v1_7_R1")) {
+			IeZP = new R1_7_R1();
+		} else if (version.equals("v1_7_R2")) {
+			IeZP = new R1_7_R2();
+		} else if (version.equals("v1_7_R3")) {
+			IeZP = new R1_7_R3();
+		} else if (version.equals("v1_7_R4")) {
+			IeZP = new R1_7_R4();
+		} else if (version.equals("v1_8_R1")) {
+			IeZP = new R1_8_R1();
+		} else if (version.equals("v1_8_R2")) {
+			IeZP = new R1_8_R2();
+		} else if (version.equals("v1_8_R3")) {
+			IeZP = new R1_8_R3();
+		} else if (version.equals("v1_9_R1")) {
+			IeZP = new R1_9_R1();
+		} else if (version.equals("v1_9_R2")) {
+			IeZP = new R1_9_R2();
+		} else if (version.equals("v1_10_R1")) {
+			IeZP = new R1_10_R1();
+		} else if (version.equals("v1_11_R1")) {
+			IeZP = new R1_11_R1();
+		} else if (version.equals("v1_12_R1")) {
+			IeZP = new R1_12_R1();
+		}
+		// This will return true if the server version was compatible with one of our NMS classes
+		// because if it is, our IeZP would not be null
+		return IeZP != null;
+	}
 }
