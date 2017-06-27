e2a87a913ec4e357cfc2bb9c6beb4d412e771a64
dvargas135 <dvargas135@users.noreply.github.com>
2017-06-27 13:26:49 -0500

    Update and rename ez/plugins/dan/SpigotUpdater/UpdateChecker.java to ez/plugins/dan/Updater/SpigotUpdateChecker.java

--- ez/plugins/dan/Updater/SpigotUpdateChecker.java
+++ ez/plugins/dan/Updater/SpigotUpdateChecker.java
@@ -20,53 +20,55 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package ez.plugins.dan.SpigotUpdater;
+package ez.plugins.dan.Updater;
 
 import java.util.logging.Logger;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
 import ez.plugins.dan.Main;
+import ez.plugins.dan.Updater.Results.UpdateResult;
 
-public class UpdateChecker {
+public class SpigotUpdateChecker {
 	private static Logger log = Logger.getLogger("Minecraft");
 	private static FileConfiguration config = Main.getPlugin().getConfig();
 	public static String version;
+	public static String oldVersion = Main.getPlugin().getDescription().getVersion();
 	
 	public static void checkUpdate() {
-    	if (config.getBoolean("updater")){
-    		log.info("Checking update...");
-    		final SpigotUpdater updater = new SpigotUpdater(Main.getJavaPlugin(), 12663, false);
-    		final SpigotUpdater.UpdateResult result = updater.getResult();
-    		switch (result) {
-    			case FAIL_SPIGOT: {
-    				log.info("The update manager could not contact Spigot.");
-    				break;
-    			}
-    			case BAD_RESOURCEID: {
-    				log.info("The update manager could not contact Spigot.");
-    				break;
-    			}
-    			case NO_UPDATE: {
-    				log.info("eZProtector is up to date.");
-    				break;
-    			}
-    			case UPDATE_AVAILABLE: {
-    				UpdateChecker.version = updater.getVersion();
-    				log.info("--------------------------");
-    				log.info("eZProtector Update Manager");
-    				log.info("An update for eZProtector is available.");
-    				log.info("Current: " + Main.getPlugin().getDescription().getVersion());
-    				log.info("New: " + version);
-    				log.info("Download it at: http://bit.ly/eZProtector");
-    				log.info("--------------------------");
-    				SpigotUpdater.updateAvailable = true;
-    				break;
-    			} default: {
-    				log.info(result.toString());
-    				break;
-    			}
-    		}
-    	}
-    }
+		if (config.getBoolean("updater")) {
+			log.info("Checking update...");
+			final SpigotUpdater updater = new SpigotUpdater(Main.getJavaPlugin(), 12663, false);
+			final UpdateResult result = updater.getResult();
+			switch (result) {
+				case FAIL_SPIGOT: {
+					log.info("[eZProtector] The update manager could not contact Spigot.");
+					break;
+				}
+				case BAD_RESOURCEID: {
+					log.info("[eZProtector] The update manager could not contact Spigot.");
+					break;
+				}
+				case NO_UPDATE: {
+					log.info("[eZProtector] The plugin is up to date.");
+					break;
+				}
+				case UPDATE_AVAILABLE: {
+					SpigotUpdateChecker.version = updater.getVersion();
+					log.info("--------------------------");
+					log.info("eZProtector Update Manager");
+					log.info("An update for eZProtector is available.");
+					log.info("Current: " + oldVersion);
+					log.info("New: " + version);
+					log.info("Download it at: http://bit.ly/eZProtector");
+					log.info("--------------------------");
+					SpigotUpdater.updateAvailable = true;
+					break;
+				} default: {
+					log.info(result.toString());
+					break;
+				}
+			}
+		}
+	}
 }
