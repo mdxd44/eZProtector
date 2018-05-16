3c9883cfe0db90b83cc3271a6d5da7c70e2e90c7
Krasimir Velkov <kgvelkov@gmail.com>
2018-05-16 17:25:21 +0300

    Add whitelist for hidden syntax blocking This was requested by an eZProtector user in issue #5!

--- src/main/java/com/github/donotspampls/ezprotector/ModLogger.java
+++ src/main/java/com/github/donotspampls/ezprotector/ModLogger.java
@@ -12,16 +12,16 @@ class ModLogger {
     private static FileConfiguration config = Main.getPlugin().getConfig();
 
     static void logMods() {
-        if (config.getBoolean("mods.5zig.block")) { logMod("5Zig"); }
-        if (config.getBoolean("mods.betterpvp.block")) { logMod("BetterPvP"); }
-        if (config.getBoolean("mods.bettersprinting.block")) { logMod("BetterSprinting"); }
-        if (config.getBoolean("mods.damageindicators.block")) { logMod("DamageIndicators"); }
-        if (config.getBoolean("mods.forge.block")) { logMod("Forge"); }
-        if (config.getBoolean("mods.liteloader.block")) { logMod("LiteLoader"); }
-        if (config.getBoolean("mods.reiminimap.block")) { logMod("Rei's Minimap"); }
-        if (config.getBoolean("mods.schematica.block")) { logMod("Schematica"); }
-        if (config.getBoolean("mods.smartmoving.block")) { logMod("SmartMoving"); }
-        if (config.getBoolean("mods.voxelmap.block")) { logMod("VoxelMap"); }
+        if (config.getBoolean("mods.5zig.block")) logMod("5Zig");
+        if (config.getBoolean("mods.betterpvp.block")) logMod("BetterPvP");
+        if (config.getBoolean("mods.bettersprinting.block")) logMod("BetterSprinting");
+        if (config.getBoolean("mods.damageindicators.block")) logMod("DamageIndicators");
+        if (config.getBoolean("mods.forge.block")) logMod("Forge");
+        if (config.getBoolean("mods.liteloader.block")) logMod("LiteLoader");
+        if (config.getBoolean("mods.reiminimap.block")) logMod("Rei's Minimap");
+        if (config.getBoolean("mods.schematica.block")) logMod("Schematica");
+        if (config.getBoolean("mods.smartmoving.block")) logMod("SmartMoving");
+        if (config.getBoolean("mods.voxelmap.block")) logMod("VoxelMap");
     }
 
     private static void logMod(String mod) {
--- src/main/java/com/github/donotspampls/ezprotector/utilities/HiddenSyntaxes.java
+++ src/main/java/com/github/donotspampls/ezprotector/utilities/HiddenSyntaxes.java
@@ -12,6 +12,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
+import java.util.List;
+
 public class HiddenSyntaxes {
 
     public static void execute(PlayerCommandPreprocessEvent event) {
@@ -24,21 +26,24 @@ public class HiddenSyntaxes {
 
         Main.errorMessage = config.getString("hidden-syntaxes.error-message");
         Main.playerCommand = command.replace("/", "");
+        List<String> whitelisted = config.getStringList("hidden-syntaxes.whitelisted");
         notifyMessage = config.getString("hidden-syntaxes.notify-admins.message");
         punishCommand = config.getString("hidden-syntaxes.punish-player.command");
         if (command.split(" ")[0].contains(":")) {
             if (!player.hasPermission("ezprotector.bypass.command.hiddensyntax")) {
-                event.setCancelled(true);
-                if (!Main.errorMessage.trim().equals("")) {
-                    player.sendMessage(Main.placeholders(Main.errorMessage));
-                }
+                if (!whitelisted.contains(command.toLowerCase().replace("/", ""))) {
+                    event.setCancelled(true);
+                    if (!Main.errorMessage.trim().equals("")) {
+                        player.sendMessage(Main.placeholders(Main.errorMessage));
+                    }
 
-                if (config.getBoolean("hidden-syntaxes.punish-player.enabled")) {
-                    Bukkit.dispatchCommand(console, Main.placeholders(punishCommand));
-                }
+                    if (config.getBoolean("hidden-syntaxes.punish-player.enabled")) {
+                        Bukkit.dispatchCommand(console, Main.placeholders(punishCommand));
+                    }
 
-                if (config.getBoolean("hidden-syntaxes.notify-admins.enabled")) {
-                    ExecutionUtil.notifyAdmins(notifyMessage, "ezprotector.notify.command.hiddensyntax");
+                    if (config.getBoolean("hidden-syntaxes.notify-admins.enabled")) {
+                        ExecutionUtil.notifyAdmins(notifyMessage, "ezprotector.notify.command.hiddensyntax");
+                    }
                 }
             }
         }
