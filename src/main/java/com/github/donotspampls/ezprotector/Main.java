e1e8379a9bbd183fc45bfa46ae1b0d493305a0d3
DoNotSpamPls <7570108+DoNotSpamPls@users.noreply.github.com>
2018-12-31 16:51:55 +0200

    eZProtector no longer supports versions lower than 1.12

--- src/main/java/com/github/donotspampls/ezprotector/Main.java
+++ src/main/java/com/github/donotspampls/ezprotector/Main.java
@@ -62,83 +62,83 @@ public class Main extends JavaPlugin {
         prefix = color(getConfig().getString("prefix"));
         String version = getServer().getVersion();
 
-        if (!version.contains("1.12") || !version.contains("1.13")) {
+        if (!version.contains("1.12") && !version.contains("1.13")) {
             getLogger().severe("eZProtector is not supported on versions lower than 1.12.2!");
             getServer().getPluginManager().disablePlugin(this);
-        }
-
-        // Save the default config
-        saveDefaultConfig();
-
-        ByteMessageListener bml = new ByteMessageListener();
-
-        // Check if the server is 1.13 or above
-        boolean newerversion;
-        try {
-            Class.forName("org.bukkit.entity.Dolphin");
-            newerversion = true;
-        } catch (ClassNotFoundException ignored) {
-            newerversion = false;
-        }
-
-        // Set mod channels (Forge 1.13 doesn't exist yet so we don't bother with most 1.13 mods)
-        if (!newerversion) {
-            ZIG = "5zig_Set";
-            BSM = "BSM";
-            MCBRAND = "MC|Brand";
-            SCHEMATICA = "schematica";
-            WDLINIT = "WDL|INIT";
-            WDLCONTROL = "WDL|CONTROL";
-
-            getServer().getMessenger().registerIncomingPluginChannel(this, ZIG, bml);
-            getServer().getMessenger().registerIncomingPluginChannel(this, BSM, bml);
-            getServer().getMessenger().registerIncomingPluginChannel(this, MCBRAND, bml);
-            getServer().getMessenger().registerIncomingPluginChannel(this, SCHEMATICA, bml);
-            getServer().getMessenger().registerIncomingPluginChannel(this, WDLINIT, bml);
-
-            getServer().getMessenger().registerOutgoingPluginChannel(this, ZIG);
-            getServer().getMessenger().registerOutgoingPluginChannel(this, BSM);
-            getServer().getMessenger().registerOutgoingPluginChannel(this, SCHEMATICA);
-            getServer().getMessenger().registerOutgoingPluginChannel(this, WDLCONTROL);
-
-            getServer().getPluginManager().registerEvents(new TabCompletionListener(), this);
         } else {
-            ZIG = "dev:null";
-            BSM = "dev:null";
-            MCBRAND = "minecraft:brand";
-            SCHEMATICA = "dev:null";
-            WDLINIT = "wdl:init";
-            WDLCONTROL = "wdl:control";
+            // Save the default config
+            saveDefaultConfig();
 
-            getServer().getMessenger().registerIncomingPluginChannel(this, ZIG, bml);
-            getServer().getMessenger().registerIncomingPluginChannel(this, MCBRAND, bml);
-            getServer().getMessenger().registerIncomingPluginChannel(this, WDLINIT, bml);
+            ByteMessageListener bml = new ByteMessageListener();
 
-            getServer().getMessenger().registerOutgoingPluginChannel(this, ZIG);
-            getServer().getMessenger().registerOutgoingPluginChannel(this, WDLCONTROL);
+            // Check if the server is 1.13 or above
+            boolean newerversion;
+            try {
+                Class.forName("org.bukkit.entity.Dolphin");
+                newerversion = true;
+            } catch (ClassNotFoundException ignored) {
+                newerversion = false;
+            }
 
-            getServer().getPluginManager().registerEvents(new BrigadierListener(), this);
+            // Set mod channels (Forge 1.13 doesn't exist yet so we don't bother with most 1.13 mods)
+            if (!newerversion) {
+                ZIG = "5zig_Set";
+                BSM = "BSM";
+                MCBRAND = "MC|Brand";
+                SCHEMATICA = "schematica";
+                WDLINIT = "WDL|INIT";
+                WDLCONTROL = "WDL|CONTROL";
+
+                getServer().getMessenger().registerIncomingPluginChannel(this, ZIG, bml);
+                getServer().getMessenger().registerIncomingPluginChannel(this, BSM, bml);
+                getServer().getMessenger().registerIncomingPluginChannel(this, MCBRAND, bml);
+                getServer().getMessenger().registerIncomingPluginChannel(this, SCHEMATICA, bml);
+                getServer().getMessenger().registerIncomingPluginChannel(this, WDLINIT, bml);
+
+                getServer().getMessenger().registerOutgoingPluginChannel(this, ZIG);
+                getServer().getMessenger().registerOutgoingPluginChannel(this, BSM);
+                getServer().getMessenger().registerOutgoingPluginChannel(this, SCHEMATICA);
+                getServer().getMessenger().registerOutgoingPluginChannel(this, WDLCONTROL);
+
+                getServer().getPluginManager().registerEvents(new TabCompletionListener(), this);
+            } else {
+                ZIG = "dev:null";
+                BSM = "dev:null";
+                MCBRAND = "minecraft:brand";
+                SCHEMATICA = "dev:null";
+                WDLINIT = "wdl:init";
+                WDLCONTROL = "wdl:control";
+
+                getServer().getMessenger().registerIncomingPluginChannel(this, ZIG, bml);
+                getServer().getMessenger().registerIncomingPluginChannel(this, MCBRAND, bml);
+                getServer().getMessenger().registerIncomingPluginChannel(this, WDLINIT, bml);
+
+                getServer().getMessenger().registerOutgoingPluginChannel(this, ZIG);
+                getServer().getMessenger().registerOutgoingPluginChannel(this, WDLCONTROL);
+
+                getServer().getPluginManager().registerEvents(new BrigadierListener(), this);
+            }
+
+            PluginCommand command = getCommand("ezp");
+            command.setExecutor(new EZPCommand());
+            command.setTabCompleter(new EZPTabCompleter());
+
+            getServer().getPluginManager().registerEvents(new CommandEventListener(), this);
+            getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+
+            // Register the metrics class and add custom charts
+            Metrics metrics = new Metrics(this);
+
+            registerMetricsChart(metrics, "updater_enabled", "updater");
+            registerMetricsChart(metrics, "tab_completion", "tab-completion.blocked");
+            registerMetricsChart(metrics, "hidden_syntaxes", "hidden-syntaxes.blocked");
+            registerMetricsChart(metrics, "custom_plugins", "custom-plugins.enabled");
+            registerMetricsChart(metrics, "custom_version", "custom-version.enabled");
+            registerMetricsChart(metrics, "custom_commands", "custom-commands.blocked");
+
+            // Initiate a (very) simple check to see if the plugin has an update!
+            if (getConfig().getBoolean("updater")) checkVersion();
         }
-
-        PluginCommand command = getCommand("ezp");
-        command.setExecutor(new EZPCommand());
-        command.setTabCompleter(new EZPTabCompleter());
-
-        getServer().getPluginManager().registerEvents(new CommandEventListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        
-        // Register the metrics class and add custom charts
-        Metrics metrics = new Metrics(this);
-
-        registerMetricsChart(metrics, "updater_enabled", "updater");
-        registerMetricsChart(metrics, "tab_completion", "tab-completion.blocked");
-        registerMetricsChart(metrics, "hidden_syntaxes", "hidden-syntaxes.blocked");
-        registerMetricsChart(metrics, "custom_plugins", "custom-plugins.enabled");
-        registerMetricsChart(metrics, "custom_version", "custom-version.enabled");
-        registerMetricsChart(metrics, "custom_commands", "custom-commands.blocked");
-
-        // Initiate a (very) simple check to see if the plugin has an update!
-        if (getConfig().getBoolean("updater")) checkVersion();
     }
 
     private void registerMetricsChart(Metrics metrics, String metricsId, String configKey) {
